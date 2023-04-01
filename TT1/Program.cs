893c5635ccd446ec2c48359b3e5a0e69cfadc2d4
Alexander <fiireex@yandex.ru>
2023-04-01 19:33:16 +0300

    done tt1

---
+++ TT1/Program.cs
@@ -0,0 +1,29 @@
+﻿void DVUMAS (double[,] mas)
+{
+        for(int i = 0; i < mas.GetLength(0); i++)
+    {
+            for(int j = 0; j < mas.GetLength(1); j++)
+            mas[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+    }
+}
+
+void POKADVUMAS (double[,] mas)
+{
+        for(int i = 0; i < mas.GetLength(0); i++)
+    {
+            for(int j = 0; j < mas.GetLength(1); j++)
+            Console.Write($"{mas[i,j]} \t");
+            Console.WriteLine();
+    }   
+}
+
+
+
+Console.Clear();
+Console.Write("Введите размерность массива: ");
+int[] s = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
+double[,]mas = new double[s[0], s[1]];
+DVUMAS(mas);
+POKADVUMAS(mas);
+
+
